85b55d7883a4396878a00910d16debf8af1c2c78
Tim Poulsen <tim@skypanther.com>
2012-02-09 10:41:59 -0500

    Testing new feed source in RSSReader

--- RSSReader/Resources/app.js
+++ RSSReader/Resources/app.js
@@ -3,7 +3,10 @@ var rr = {}; // create our namespace rr = Rss Reader
 // require the module files
 rr.ui = require('ui');
 // require in the network library and fetch the RSS feed
-require('network').fetchRSSData('http://www.rit.edu/news/lib/rss/inthenews.rss');
+
+require('network').fetchRSSData('http://v2.0.news.tmg.s3.amazonaws.com/feeds/news.xml');
+
+//require('network').fetchRSSData('http://www.rit.edu/news/lib/rss/inthenews.rss');
 
 // open the app's UI
 rr.tabGroup = rr.ui.createAppTabs();
